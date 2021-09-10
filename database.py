e7823578c2895fe25c3777c5ea45d38c8ba9e85d
rabibasukala01 <63946883+rabibasukala01@users.noreply.github.com>
2021-09-10 17:36:08 +0545

    testing phase

---
+++ database.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+
+def connect(guild_id):
+    connection = sqlite3.connect(f'{guild_id}.db')
+    cursor = connection.cursor()
+    return cursor
+
+
+def create(guild_id):
+    cursor = connect(guild_id)
+
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS PEOPLE (id INTEGER PRIMARY KEY,NAME STRING ,MONTH INTEGER ,DAY INTEGER )")
+
+
+def add_people(guild_id, name, month, day):
+    cursor = connect(guild_id)
+    create(guild_id)
+    cursor.execute(
+        "INSERT INTO PEOPLE (NAME,MONTH ,DAY)VALUES(?,?,?);", (name, month, day))
+    # cause connection halyo vaney ta not define vai halxa ni
+    sqlite3.connect(f'{guild_id}.db').commit()
+
+
+def today_bday(today_month, today_day, guild_id):
+    cursor = connect(guild_id)
+    create(guild_id)
+    newList = []
+
+    NAMES = cursor.execute(
+        "SELECT NAME FROM PEOPLE WHERE MONTH=? AND DAY =?;", (today_month, today_day))
+
+    for name in NAMES:
+        newList.append(name[0])
+        print(name)
+    return newList
+
+
+# def debugshow():
+#     return cursor.execute("SELECT * FROM PEOPLE").fetchall()
---
+++ main.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext import commands
+import database
+import datetime
+
+client = commands.Bot(command_prefix='?', intents=discord.Intents.all())
+
+print("test")
+
+
+@client.event
+async def on_ready():
+    print("ok")
+
+
+@client.command()
+async def addmine(context, month, day, *, name):
+
+    guild_id = context.message.guild.id
+
+    database.add_people(str(guild_id), name, month, day)
+    await context.send("BIRTH DATE ADDED TO DATABASE")
+
+
+@client.command()
+async def todaybirthday(context):
+    date_object = datetime.date.today()
+    dates = (str(date_object).split("-"))  # yyyy-mm-dd
+
+    guild_id = context.message.guild.id
+
+    data_lists = database.today_bday(
+        int(dates[1]), int(dates[2]), str(guild_id))
+    # data_lists = database.today_bday(9, 7)
+    # print(data_lists)
+
+    if len(data_lists) == 0:
+        await context.send("NO ONE BORN ON THIS DAY LMAO")
+    else:
+        for data in data_lists:
+            await context.send(data)
+
+
+client.run("ODg1NTE3MjQ2NzM3MjkzMzQy.YToMMw.Js5Xv7KzAzl5AA35J0feCAZtuV4")
