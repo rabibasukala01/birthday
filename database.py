92dc22ec7f85b1227bdabfe321f906709da190d5
rabibasukala01 <63946883+rabibasukala01@users.noreply.github.com>
2021-09-15 18:00:30 +0545

    update add_people function
    people can add their date one time only but there is update function if you want to update date lol

--- database.py
+++ database.py
@@ -13,16 +13,23 @@ def create(guild_id, connection):
 
     with connection:
         connection.execute(
-            "CREATE TABLE IF NOT EXISTS PEOPLE (SN INTEGER PRIMARY KEY,ID INTEGER ,NAME STRING ,DISCRIMINATOR STRING ,MONTH INTEGER ,DAY INTEGER ,ADDED BOOL)")
+            "CREATE TABLE IF NOT EXISTS PEOPLE (SN INTEGER PRIMARY KEY,ID INTEGER ,NAME STRING ,DISCRIMINATOR STRING ,MONTH INTEGER ,DAY INTEGER );")
 
 
-def add_people(guild_id, month, day, name, discriminator, id_of_member, added):
+def add_people(guild_id, month, day, name, discriminator, id_of_member):
     connection = connect_to_db(guild_id)
-
+    create(guild_id, connection)
     with connection:
-        create(guild_id, connection)
-        connection.execute(
-            "INSERT INTO PEOPLE (ID ,NAME,DISCRIMINATOR,MONTH ,DAY,ADDED)VALUES(?,?,?,?,?,?);", (id_of_member, name, discriminator, month, day, added))
+
+        TEST_IF_ID_AVAILABLE = connection.execute(
+            "SELECT ID FROM PEOPLE WHERE ID=?;", (id_of_member,))
+
+        if TEST_IF_ID_AVAILABLE.fetchall() != []:
+            return "ALREADY IN DATABASE TRY UPDATING DATE IF ENTERED INCORRECTLY"
+        else:
+            connection.execute(
+                "INSERT INTO PEOPLE (ID ,NAME,DISCRIMINATOR,MONTH ,DAY)VALUES(?,?,?,?,?);", (id_of_member, name, discriminator, month, day))
+            return "ADDED TO DATABASE"
 
     connection.commit()
 
@@ -35,7 +42,7 @@ def today_bday(today_month, today_day, guild_id):
     with connection:
 
         IDlists = connection.execute(
-            "SELECT ID,ADDED FROM PEOPLE WHERE MONTH=? AND DAY =?;", (today_month, today_day))
+            "SELECT ID FROM PEOPLE WHERE MONTH=? AND DAY =?;", (today_month, today_day))
 
         for lists in IDlists:
             newIDlists.append(lists)  # each element in list is tuple
@@ -60,3 +67,12 @@ def update_data(guild_id, memberID, month, day):
 
 
 # today_bday(9, 13, 885502487346417695)
+# connection = connect_to_db(871619898852393070)
+# with connection:
+#     create(871619898852393070, connection)
+#     TEST_IF_ID_AVAILABLE = connection.execute(
+#         "SELECT ID FROM PEOPLE WHERE ID=?;", (717730586843938846,))
+
+# print(TEST_IF_ID_AVAILABLE.fetchall())
+# if TEST_IF_ID_AVAILABLE.fetchall() == []:
+#     print("lol")
